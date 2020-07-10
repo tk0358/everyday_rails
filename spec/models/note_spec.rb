4b133a83bda96a1c5a97de82b6c9ea30c03143a9
tk0358 <tk0358a@yahoo.co.jp>
2020-07-10 11:16:43 +0900

    Add mock and stab

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -64,4 +64,18 @@ RSpec.describe Note, type: :model do
     puts "This note's project is #{note.project.inspect}"
     puts "This note's user is #{note.user.inspect}"
   end
+  
+  it "delegates name to the user who created it" do
+    # 通常版
+    # user = FactoryBot.create(:user, first_name: "Fake", last_name: "User")
+    # note = Note.new(user: user)
+    # expect(note.user_name).to eq "Fake User"
+
+    # mockを使った版
+    # user = double("user", name: "Fake User")
+    user = instance_double("User", name: "Fake User")
+    note = Note.new
+    allow(note).to receive(:user).and_return(user)
+    expect(note.user_name).to eq "Fake User"
+  end
 end
